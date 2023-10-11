5d94b02731e333ac39c03da847cb88d4abe3d49a
fersonpriadi <fersonpriadi99@gmail.com>
2023-10-11 21:44:58 +0700

    update controller

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class Controller extends BaseController{
+class LoginController extends Controller
+{
     /**
      * Handle an authentication attempt.
      */
